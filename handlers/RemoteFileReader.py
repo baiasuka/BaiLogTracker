a1cb3169102bdb06dd3593a4e02c7c4d590f0b0f
Bai_Asuka <sonoda_umi@163.com>
2019-09-04 18:49:10 +0800

    新增远程文件访问函数

---
+++ handlers/RemoteFileReader.py
@@ -0,0 +1,27 @@
+import paramiko
+
+class FtpCilent:
+    def __init__(self, host, port, username, password, timeout=5):
+        self._host = host
+        self._port = port
+        self._username = username
+        self._password = password
+        self._timeout = timeout
+
+    def __enter__(self):
+        self._cilent = paramiko.SSHClient()
+        self._cilent.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self._cilent.connect(self._host, self._port, self._username, self._password)
+        self._sftp_cilent = self._cilent.open_sftp()
+        return self._sftp_cilent
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._cilent.close()
+
+
+if __name__ == '__main__':
+    with FtpCilent('47.102.106.106', 22, 'root', 'Ang@318Hwj') as cilent:
+        f = cilent.open('/www/HongXiangLogSys/testlog.txt')
+        result = f.readline()
+        print(result)
+        f.close()
